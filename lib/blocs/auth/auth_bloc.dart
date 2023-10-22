34fecd143f1ce4efbe9ab5532328af03d0a87ab2
Admin <btaylor@allytechllc.com>
2023-10-22 15:02:06 -0400

    remove api file

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 
 part 'auth_event.dart';
 part 'auth_state.dart';
--- lib/blocs/auth/auth_state.dart
+++ lib/blocs/auth/auth_state.dart
@@ -1,3 +1,4 @@
+
 part of 'auth_bloc.dart';
 
 enum AuthStatus {
@@ -5,3 +6,30 @@ enum AuthStatus {
   authenticated,
   unauthenticated,
 }
+
+class AuthState extends Equatable {
+  final AuthStatus authStatus; //enum AuthStatus
+  final fbAuth.User? user; // in AuthBLoc: import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
+  const AuthState({
+    required this.authStatus,
+    this.user,
+  });
+
+  @override
+  List<Object?> get props => [authStatus, user];
+
+  @override
+  String toString() => 'AuthState(authStatus: $authStatus, user: $user)';
+
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    fbAuth.User? user,
+  }) {
+    return AuthState(
+      authStatus: authStatus ?? this.authStatus,
+      user: user ?? this.user,
+    );
+  } //AuthState
+  
+} //class AuthState
+

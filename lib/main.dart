cfbfe7daa920ba168b7d1823aa8f72c572475286
Admin <btaylor@allytechllc.com>
2023-10-23 15:39:04 -0400

    DONE: step 81

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/blocs/signin/signin_cubit.dart';
 import 'package:fb_auth_bloc/pages/home_page.dart';
 import 'package:fb_auth_bloc/pages/signin_page.dart';
 import 'package:fb_auth_bloc/pages/signup_page.dart';
@@ -36,8 +37,13 @@ class MyApp extends StatelessWidget {
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(
+          BlocProvider<AuthBloc>(
             create: (context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider<SigninCubit>(
+            create: (context) => SigninCubit(
               authRepository: context.read<AuthRepository>(),
             ),
           ),
